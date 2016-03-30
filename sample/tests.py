227bfe832e0895792cd56fad7803363abff3a5bc
Jasim <jasim90@gmail.com>
2016-03-30 12:39:53 +0530

    added test

--- sample/tests.py
+++ sample/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from models import Person
+from django.contrib.auth.models import User
 
 # Create your tests here.
+
+
+class PersonTestCase(TestCase):
+
+    def setUp(self):
+        auth = User.objects.create(username="Test", email="test@tests.com")
+        Person.objects.create(
+            first_name="Test", last_name="last_name", user=auth)
+
+    def test_person(self):
+        name = Person.objects.get(first_name="Test")
